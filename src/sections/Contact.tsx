cde60e651592e91fd92b316d5d6a5dcdf2d05d2f
yogesh kumar saini <159942464+digitalwavekt@users.noreply.github.com>
2026-07-14 14:15:17 +0530

    contactu data update

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -265,7 +265,7 @@ export default function Contact() {
             {/* Map Embed */}
             <div className="rounded-xl overflow-hidden border border-white/10 aspect-video">
               <iframe
-                src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3305.715220363292!2d-118.4005473!3d34.0736203!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2bc04d6d14723%3A0xd6cac5b8a0b7d39c!2sBeverly%20Hills%2C%20CA%2090210!5e0!3m2!1sen!2sus!4v1`}
+                src={`https://www.google.com/maps?q=${encodeURIComponent(address)}&output=embed`}
                 width="100%"
                 height="100%"
                 style={{ border: 0, filter: "invert(90%) hue-rotate(180deg)" }}
